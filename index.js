b499d862c906f543042f600427e1e4941c9258bd
Kyle Chart <kyliechart@yahoo.com>
2023-02-10 13:23:26 +0000

    If else If else

--- index.js
+++ index.js
@@ -44,16 +44,74 @@ if (h < k) {
 
 console.log(result);
 
+// If else statments can be chanined together resulting in nested chained if else statments, allowing multiple conditional statments
+
+// If else If else statments - Chaining them together with them selves. 
+
+var age = 15;
+var drink 
+
+if (age > 21) {
+    drink = "drink whiskey";
+} else if (age >= 18) {
+    drink = "drink beer";
+} else if (age >= 12) {
+    drink = "drink cola";
+} else {
+    drink = "drink apple juice";
+}
+
+console.log(drink);
+
+// Nested if else if else statments 
+// Based on the two variables it logs a certain string. And if nothing matches it will log the final else statment. 
+
+let user = true;
+let emailVerified = false;
+
+if (user === true) {
+    if (emailVerified === true) {
+        console.log("Welcome to our web-site")
+    } else {
+        console.log("Please verify your email")
+    }
+} else {
+    console.log("You need to be a user to visit this page")
+}
+
+// Dependent of the age variable, it will change the log output to the console. Based on the if else statments. 
+
 //Ternary Expressions
 
-let a = 10;
-let b = 20;
-let result;
+let f = 10;
+let d = 20;
+let output;
 
 // Write your code below this line. 
 
-result = (a > b) ? "a is smaller" : "a is not smaller"; 
+result = (f > d) ? "f is smaller" : "f is not smaller"; 
 // Compares the two variables together and based on the date logs one of the other strings. 
 
-console.log(result);
+console.log(output);
 
+// Switch case statments
+// Changes the output dependent of the varibles data. If it matches it outputs that specific data match 
+
+let chocolate = ''; 
+let errorType = "page";
+
+switch (errorType) {
+    case "username":
+        chocolate = "That username is incorrect, please try again.";
+        break; // Important to break after every statment, as if it matches it will stop the code and output the correct data
+    case "password":
+        chocolate = "Incorrect password, please try again.";
+        break;
+    case "page": // The case is what it is searching for in it matching process to find the correct output needed. 
+        chocolate = "Sorry this page doesn't exist.";
+        break;
+    default: // If nothing matches it will output the defualt message
+        chocolate = "Error message unknown";
+}
+
+console.log(chocolate);
