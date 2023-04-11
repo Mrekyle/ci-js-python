39b27f2d23ea05cb4d03def30601f72a9d4cbadc
mrekyle <kylechart@gmail.com>
2023-04-11 11:41:27 +0100

    Change file Structure. Add Python lessons/notes

---
+++ functions.py
@@ -0,0 +1,8 @@
+# Defining functions in Python is different to JS. Firstly to define a function you use the def keyword and then the functions name.
+# They can also take different paramaters like JS inside the brackets. Then by using indentation you tell the language that the code 
+# belongs to the function above. 
+
+def print_message():
+    print('Hello World')
+
+print_message() # Calls the function of print message. Similar to the way JS Functions are called
---
+++ python/python.py
@@ -0,0 +1,26 @@
+# Same as console.log in JS. Using print statments throughout the code is a good way to catch errors and to see what is going on quickly
+
+print('Hello World')
+
+# System Exit cuts the code and exits the application safely. Just like powering down a pc using the correct process not pulling the plug out
+
+raise SystemExit('Some error Message')
+# The above pauses and exits the code based on the statement before its process. Allowing us to look at the errors/place it is at in the code to 
+# help catch any bugs or errors 
+
+# Errors
+
+#print 'Hello, World!' Will throw an error as its not correct syntax for the python language. There mut be the brackets in the statment for it to 
+# read the code and print the desired outcome
+
+# Variables are defined differently to JS. They are also case sensitive. They can also contain spaces, by using an underscore. Meaning.
+
+myvar = 5 
+my_var = 5
+# Is a different variable to 
+myVar = 5
+my_Var = 5
+# all of the above variables are compleatly different despite essentially having the same names and values assigned to them
+
+# True/False
+# In Python Booleans are defined by using the true and false keywords. But the first letter of each must be capataised
