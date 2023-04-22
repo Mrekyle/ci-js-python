2d0cd1a291524f9cee055cdfc5fa525815822192
Kyle Chart <64297454+Mrekyle@users.noreply.github.com>
2023-04-22 17:38:26 +0100

    Add passing methods around

--- python/classes.py
+++ python/classes.py
@@ -183,3 +183,32 @@ class Electric(Vehicle):
         
 volkswagen = InternalCombustion("Volkswagen", "Golf", 1.7, "Diesel")
 tesla = Electric("Tesla", "X")
+
+"""
+Passing methods between classes.
+"""
+
+class Employee():
+    """
+    Creates an instance of Employee
+    """
+    def __init__(self, name, annual_salary):
+        self.name = name
+        self.annual_salary = annual_salary
+        
+    def calculate_monthly_salary(self):
+        return self.annual_salary / 12
+        
+class CustomerServiceEmployee(Employee):
+    """
+    Creates an instance of CustomerServiceEmployee
+    """
+    def __init__(self, name, annual_salary, department):
+        super().__init__(name, annual_salary)
+        self.department = department
+            
+cs_manager = CustomerServiceEmployee("Kelly Johnson", 42000, "Customer Service")
+
+kellys_monthly_salary = cs_manager.calculate_monthly_salary()
+
+print(kellys_monthly_salary)
